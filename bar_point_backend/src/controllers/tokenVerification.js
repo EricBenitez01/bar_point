b861651a5b25afaca7e8bfbeb193ad20b97df9d0
toledoardev <alejandrotoledodev@gmail.com>
2023-09-29 21:04:07 -0300

    Refactorización de middleware tokenVerification y prueba del mismo en get users route

--- bar_point_backend/src/controllers/tokenVerification.js
+++ bar_point_backend/src/controllers/tokenVerification.js
@@ -2,23 +2,32 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     // Accedo al token enviado en el header de la solicitud
-    const token = req.header('Authorization');
+    const headerToken = req.headers['authorization'];
 
-    if (!token) {
-        return res.status(401).json({ 
-            msg: 'Token not found, access denied' 
+    if ( headerToken != undefined && headerToken.startsWith('Bearer ') ) {
+        // headerToken tiene el siguiente formato: 'Bearer token'
+        // Se obtiene el token
+        const bearerToken = headerToken.split(' ');
+
+        try {
+
+            // Se verifica el token
+            jwt.verify(bearerToken[1], process.env.SECRET_TOKEN);
+
+            // Se permite el acceso a la ruta
+            next(); 
+        }   
+        catch (err) {
+            res.status(401).json({ 
+                msg: 'Invalid token, access denied' 
+            });
+        }
+
+        
+    } else {
+        res.status(401).json({ 
+            msg: 'Access denied' 
         });
-    };
-
-    try {
-
-        // Se verifica el token y devuelve el objeto correspondiente en data
-        const data = jwt.verify(token, process.env.SECRET_TOKEN);
-        // Se permite el acceso a la ruta
-        next(); 
-
-    } catch (err) {
-        res.status(401).json({ msg: 'Invalid token, access denied' });
     };
 };
 
--- bar_point_backend/src/routes/usersRoutes.js
+++ bar_point_backend/src/routes/usersRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {list, detail, create, update, destroy} = require('../controllers/usersController');
+const verifyToken = require('../controllers/tokenVerification');
 
 router
-    .get('/users', list)
+    .get('/users', verifyToken, list)
     .get('/users/:id', detail)
     .post('/users', create)
     .put('/users/:id', update)
