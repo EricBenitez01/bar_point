e9e42e7168ac0448275b8a5678d8cddd6075e2d5
EricBenitez01 <leonetkd@gmail.com>
2023-11-07 22:09:03 -0300

    r

--- bar_point_backend/src/app.js
+++ bar_point_backend/src/app.js
@@ -3,18 +3,66 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
+const app = express();
+const corsOptions = {
+    origin: 'http://localhost:4200', // Dominio de tu aplicación Angular
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 200,
+};
+
+
+
 const authRoute = require('./routes/authRoute');
 const usersRoutes = require('./routes/usersRoutes');
 const benefitsRoutes = require('./routes/benefitsRoutes');
-
-const app = express();
+//const transaction = require('./routes/transactionRoute');
+const cors = require('cors');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors(corsOptions));
+app.use(authRoute, cors(corsOptions));
+app.use(usersRoutes, cors(corsOptions));
+app.use(benefitsRoutes, cors(corsOptions));
+//app.use(purchaseRoutes, cors(corsOptions));
+//app.use(transaction, cors(corsOptions));
 
-app.use(authRoute);
-app.use(usersRoutes);
-app.use(benefitsRoutes);
 
 
 app.listen('3001', () => console.log('Server running in port 3001'));
+
+/* require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const authRoute = require('./routes/authRoute');
+const usersRoutes = require('./routes/usersRoutes');
+const benefitsRoutes = require('./routes/benefitsRoutes');
+const purchaseRoutes = require('./routes/purchaseRoutes');
+const transaction = require('./routes/transactionRoute');
+const cors = require('cors');
+
+const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:4200', // Dominio de tu aplicación Angular
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 200,
+};
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// Configurar opciones de CORS
+
+  
+// Aplicar CORS solo a la ruta /users
+app.use(authRoute, cors(corsOptions));
+app.use(usersRoutes, cors(corsOptions));
+app.use(benefitsRoutes, cors(corsOptions));
+app.use(purchaseRoutes, cors(corsOptions));
+app.use(transaction, cors(corsOptions));
+
+
+app.listen('3001', () => console.log('Server running in port 3001')); */
